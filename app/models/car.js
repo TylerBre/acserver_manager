ce7e93018b736922f6acb4d92dc4c60c92a963c6
Tyler Breland <workwith@tylerbreland.me>
2016-01-24 02:58:11 -0600

    better car content update, using orm now

---
+++ app/models/car.js
@@ -0,0 +1,32 @@
+var Waterline = require('waterline');
+
+module.exports = Waterline.Collection.extend({
+  identity: 'car',
+  connection: 'psql',
+
+  attributes: {
+    name: 'string',
+    brand: 'string',
+    description: 'text',
+    car_class: 'string',
+    power: 'string',
+    torque: 'string',
+    weight: 'string',
+    torque_curve: 'array',
+    power_curve: 'array'
+  },
+
+  fromKunos: (data) => {
+    return {
+      name: data.name,
+      brand: data.brand,
+      description: data.description,
+      car_class: data.class,
+      power: data.specs.bhp,
+      torque: data.specs.torque,
+      weight: data.specs.weight,
+      torque_curve: data.torqueCurve,
+      power_curve: data.powerCurve
+    };
+  }
+});
--- app/routes/content.js
+++ app/routes/content.js
@@ -1,4 +1,5 @@
 var promise = require('bluebird');
+var _ = require('lodash');
 var app = require('../../app.js');
 var router = require('express').Router();
 var controllers = require('../controllers');
@@ -7,8 +8,45 @@ var helpers = require('../helpers');
 
 // router.get('*', controllers.app.before);
 router.get('/cars', (req, res, next) => {
-  helpers.content.cars().then((cars) => {
-    res.render('content_cars', {cars});
+  app.models.car.find().then((cars) => res.render('content_cars', {cars}));
+});
+
+router.get('/cars/update', (req, res, next) => {
+  var updated = 0;
+  helpers.content.cars().map((_car) => {
+    var car = app.models.car.fromKunos(_car.data);
+    var criterea = {
+      name: car.name,
+      brand: car.brand
+    };
+
+    return app.models.car.findOrCreate(car).where(criterea).then((data) => {
+      // we found a match, but the data is old, we need to update it
+      if (!_.isMatch(data, car)) {
+        console.log("not match")
+        updated += 1;
+        return app.models.car.update(criterea, car);
+      }
+
+      // multiple matches in the db, delete them and add the new one
+      if (_.isArray(data)) {
+        console.log("is ary")
+        updated += 1
+        return app.models.car.destroy(data).then(() => {
+          return app.models.car.create(car);
+        });
+      }
+
+      // must be new data, or unchanged existing data
+      return data;
+    })
+  }).then(() => res.send("updated: " + updated));
+});
+
+router.get('/cars/destroy', (req, res, next) => {
+
+  app.models.car.destroy().then(() => {
+    res.redirect('/content/cars');
   });
 });
 
