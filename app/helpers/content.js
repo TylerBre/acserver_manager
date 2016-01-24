5a61b2e95f816078c90a7f8a46d3ca65947cd377
Tyler Breland <workwith@tylerbreland.me>
2016-01-24 16:34:32 -0600

    better naming convention

--- app/helpers/content.js
+++ app/helpers/content.js
@@ -1,17 +1,17 @@
-var fs = require('./fs_content.js');
+var fs_content = require('./fs_content.js');
 var path = require('path');
 
 var Content = module.exports;
 
 Content.cars = () => {
   var pwd = '/home/ubuntu/acserver/content/cars';
-  return fs.readDir(pwd)
-    .reduce(fs.directories_only(pwd), [])
-    .reduce(fs.ui_directories_only(pwd), [])
-    .reduce(fs.ui_data_only(pwd, 'ui_car.json', (item, data) => {
+  return fs_content.readDir(pwd)
+    .reduce(fs_content.directories_only(pwd), [])
+    .reduce(fs_content.ui_directories_only(pwd), [])
+    .reduce(fs_content.ui_data_only(pwd, 'ui_car.json', (item, data) => {
       return {
         'badge': path.join(pwd, item.file_name, 'badge.png'),
-        'data': JSON.parse(fs.un_format(data))
+        'data': JSON.parse(fs_content.un_format(data))
       };
     }), [])
     .catch({code: 'ENOTDIR'}, () => {});
@@ -19,14 +19,14 @@ Content.cars = () => {
 
 Content.tracks = () => {
   var pwd = '/home/ubuntu/acserver/content/tracks';
-  return fs.readDir(pwd)
-    .reduce(fs.directories_only(pwd), [])
-    .reduce(fs.ui_directories_only(pwd), [])
-    .reduce(fs.ui_data_only(pwd, 'ui_track.json', (item, data) => {
+  return fs_content.readDir(pwd)
+    .reduce(fs_content.directories_only(pwd), [])
+    .reduce(fs_content.ui_directories_only(pwd), [])
+    .reduce(fs_content.ui_data_only(pwd, 'ui_track.json', (item, data) => {
       return {
         'outline': path.join(pwd, item.file_name, 'outline.png'),
         'preview': path.join(pwd, item.file_name, 'preview.png'),
-        'data': JSON.parse(fs.un_format(data))
+        'data': JSON.parse(fs_content.un_format(data))
       };
     }), [])
 };
