bf7511470ec753746269e3094a3aca87391e7fc0
Tyler Breland <workwith@tylerbreland.me>
2016-02-04 11:34:49 -0600

    login to steamcmd using anonymous user for CI

--- bin/acserver_update.js
+++ bin/acserver_update.js
@@ -5,31 +5,45 @@ var path = require('path');
 // assumes this script is run from the project directory
 var install_dir = path.resolve('lib/acserver');
 var steamcmd_dir = path.resolve('lib/steamcmd');
-console.log("Updating Assetto Corsa Dedicated Server via steamcmd...\n");
-console.log("NOTE! You will be prompted for a steam gaurd code if this first time running this command.");
-console.log("NOTE! The Steam Guard code will be sent to the email you registered your steam account with.\n");
-prompt.get({
-  properties: {
-    uname: {
-      type: 'string',
-      required: true,
-      description: 'Your Steam username'
-    },
-    pass: {
-      type: 'string',
-      description: 'Your Steam username',
-      required: true,
-      hidden: true
-    }
-  }
-}, function (err, result) {
-  if (err) {
-    console.log('Error!');
-    throw err;
-  }
 
-  var cmd = `${path.join(steamcmd_dir, 'steamcmd.sh')} +login ${result.uname} ${result.pass} +force_install_dir ${install_dir} +@sSteamCmdForcePlatformType windows +app_update 302550 validate +quit`;
-  console.log(`Running: ${cmd.replace(result.pass, '****')}`);
+if (process.env.CI && process.env.CI == 'travis') {
+  var cmd = `${path.join(steamcmd_dir, 'steamcmd.sh')} +login anonymous +force_install_dir ${install_dir} +@sSteamCmdForcePlatformType windows +app_update 302550 validate +quit`;
+  install_steam(cmd);
+
+} else {
+  console.log("Updating Assetto Corsa Dedicated Server via steamcmd...\n");
+  console.log("NOTE! You will be prompted for a steam gaurd code if this first time running this command.");
+  console.log("NOTE! The Steam Guard code will be sent to the email you registered your steam account with.\n");
+
+  prompt.get({
+    properties: {
+      uname: {
+        type: 'string',
+        required: true,
+        description: 'Your Steam username'
+      },
+      pass: {
+        type: 'string',
+        description: 'Your Steam username',
+        required: true,
+        hidden: true
+      }
+    }
+  }, function (err, result) {
+    if (err) {
+      console.log('Error!');
+      throw err;
+    }
+
+    var cmd = `${path.join(steamcmd_dir, 'steamcmd.sh')} +login ${result.uname} ${result.pass} +force_install_dir ${install_dir} +@sSteamCmdForcePlatformType windows +app_update 302550 validate +quit`;
+    var cmd_sanitized = cmd.replace(result.pass, '****');
+    install_steam(cmd, cmd_sanitized);
+  });
+}
+
+function install_steam (cmd, cmd_sanitized) {
+  cmd_sanitized = cmd_sanitized || cmd;
+  console.log(`Running: ${cmd_sanitized}`);
   exec(cmd, {stdio: 'inherit'});
   console.log('Finished updating Assetto Corsa Dedicated Server\n');
-});
+}
