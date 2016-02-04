e7aa6c95fca8bfb03c907abf819f63de4da177a1
Tyler Breland <workwith@tylerbreland.me>
2016-02-04 12:04:05 -0600

    npm postinstall script not using steamcmd if in CI env

--- bin/acserver_update.js
+++ bin/acserver_update.js
@@ -6,11 +6,7 @@ var path = require('path');
 var install_dir = path.resolve('lib/acserver');
 var steamcmd_dir = path.resolve('lib/steamcmd');
 
-if (process.env.CI && process.env.CI == 'travis') {
-  var cmd = `${path.join(steamcmd_dir, 'steamcmd.sh')} +login anonymous +force_install_dir ${install_dir} +@sSteamCmdForcePlatformType windows +app_update 302550 validate +quit`;
-  install_steam(cmd);
-
-} else {
+if (!process.env.CI && process.env.CI !== 'travis') {
   console.log("Updating Assetto Corsa Dedicated Server via steamcmd...\n");
   console.log("NOTE! You will be prompted for a steam gaurd code if this first time running this command.");
   console.log("NOTE! The Steam Guard code will be sent to the email you registered your steam account with.\n");
