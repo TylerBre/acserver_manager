1fc24ae565c64b08c0c12eb2d1946e4e0cd2ff2b
Tyler Breland <workwith@tylerbreland.me>
2016-02-04 12:11:19 -0600

    preinstall script update, using default saved checksums for CI

--- bin/acserver_update.js
+++ bin/acserver_update.js
@@ -6,36 +6,38 @@ var path = require('path');
 var install_dir = path.resolve('lib/acserver');
 var steamcmd_dir = path.resolve('lib/steamcmd');
 
-if (!process.env.CI && process.env.CI !== 'travis') {
-  console.log("Updating Assetto Corsa Dedicated Server via steamcmd...\n");
-  console.log("NOTE! You will be prompted for a steam gaurd code if this first time running this command.");
-  console.log("NOTE! The Steam Guard code will be sent to the email you registered your steam account with.\n");
-
-  prompt.get({
-    properties: {
-      uname: {
-        type: 'string',
-        required: true,
-        description: 'Your Steam username'
-      },
-      pass: {
-        type: 'string',
-        description: 'Your Steam username',
-        required: true,
-        hidden: true
-      }
-    }
-  }, function (err, result) {
-    if (err) {
-      console.log('Error!');
-      throw err;
-    }
-
-    var cmd = `${path.join(steamcmd_dir, 'steamcmd.sh')} +login ${result.uname} ${result.pass} +force_install_dir ${install_dir} +@sSteamCmdForcePlatformType windows +app_update 302550 validate +quit`;
-    var cmd_sanitized = cmd.replace(result.pass, '****');
-    install_steam(cmd, cmd_sanitized);
-  });
+if (process.env.CI) {
+  return console.log("Detected CI environment: NOT using steamcmd. Continuing.\n");
 }
+
+console.log("Updating Assetto Corsa Dedicated Server via steamcmd...\n");
+console.log("NOTE! You will be prompted for a steam gaurd code if this first time running this command.");
+console.log("NOTE! The Steam Guard code will be sent to the email you registered your steam account with.\n");
+
+prompt.get({
+  properties: {
+    uname: {
+      type: 'string',
+      required: true,
+      description: 'Your Steam username'
+    },
+    pass: {
+      type: 'string',
+      description: 'Your Steam username',
+      required: true,
+      hidden: true
+    }
+  }
+}, function (err, result) {
+  if (err) {
+    console.log('Error!');
+    throw err;
+  }
+
+  var cmd = `${path.join(steamcmd_dir, 'steamcmd.sh')} +login ${result.uname} ${result.pass} +force_install_dir ${install_dir} +@sSteamCmdForcePlatformType windows +app_update 302550 validate +quit`;
+  var cmd_sanitized = cmd.replace(result.pass, '****');
+  install_steam(cmd, cmd_sanitized);
+});
 
 function install_steam (cmd, cmd_sanitized) {
   cmd_sanitized = cmd_sanitized || cmd;
